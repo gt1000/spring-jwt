34ecc8aa81cf12979148aafa7984ab54b7f9836c
DESKTOP-H3MKUI2\Cheon JeongDae <gt1000@gmail.com>
2022-04-18 01:38:07 +0900

    feat: 임시

--- src/main/java/gt1000/SpringJwtApplication.java
+++ src/main/java/gt1000/SpringJwtApplication.java
@@ -2,7 +2,9 @@ package gt1000;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//@EnableJpaAuditing
 @SpringBootApplication
 public class SpringJwtApplication {
 
